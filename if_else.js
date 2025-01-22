f391401428d39e6950612ba9841f3b95137fa1b9
Naomy Arnold <NaomyA@newsmax.com>
2025-01-22 12:48:24 -0500

    added if elese lesson 100-102

---
+++ if_else.js
@@ -0,0 +1,13 @@
+let loveScore = Math.random() * 100
+loveScore = Math.floor(loveScore) + 1 
+console.log("Your love score is " + loveScore + "%") 
+
+if(loveScore > 70){
+alert (" Your love score is " + loveScore + "whats up")
+if(loveScore > 30 && loveScore<= 70){
+alert (" Your love score is " + loveScore + "whats up")
+}
+}else{
+
+    alert("Your love score is " + loveScore + "whats up")
+}
