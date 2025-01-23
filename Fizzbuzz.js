3428ec1b102f111018b9f56935510fa9d4667615
Naomy Arnold <NaomyA@newsmax.com>
2025-01-23 15:42:38 -0500

    added FizzBuzz notes

--- Fizzbuzz.js
+++ Fizzbuzz.js
@@ -1,12 +1,17 @@
 
 
-// I LOSTR jobs because of this simple problem :-) 
-let output = [];
+// I LOST jobs because of this simple problem :-) 
+
+//write a function Every single time it will add number in sequence called output and print out what you want in console.
+
+let output = [];// we are keeping track of the numbers with this.
 let count = 1;
 
 
 function fizzBuzz(){
     
+
+    //% modular check remainder  if divisible by 3 it will pring Fizz and Buzz for divisible by 5
 if(count % 3 === 0 && count % 5 === 0){
   output.push("FizBuzz");
     
@@ -21,9 +26,12 @@ else if (count % 5 === 0){
 }
 else{
 
-    output.push(count);
+    output.push(count); //pushing the number to the empty array
 }
 
-   count++;
+   count++; //increment -  everytime we run it will add a  number for us to check
     console.log(output);// just number
 }
+
+
+// to run this in the council I put FizzBuzz() and ran it there
