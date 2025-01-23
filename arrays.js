09a9e0485f3491c820af523c6c7402f5e00bd61f
Naomy Arnold <NaomyA@newsmax.com>
2025-01-23 15:33:37 -0500

    added FizzBuzz and ifElse

---
+++ arrays.js
@@ -0,0 +1,21 @@
+let guesTList = ["janise", "Anna", "Naomy", "Tom"]
+
+//guest by index
+// console.log(list[0])
+// console.log(list[1])
+// console.log(list[2])
+// console.log(list[3])
+
+
+//includes
+let guestName = prompt("What is your name")
+
+if(guesTList.includes(guestName)){
+
+alert("Welcome")
+
+}else{
+
+    alert("Next time")
+}
+
---
+++ push.js
@@ -0,0 +1,15 @@
+let arrayNumbers= [3, 8, 5, 6, 9, ]
+
+let emptyArray = []
+
+emptyArray = arrayNumbers.push(55)
+console.log(arrayNumbers) // 55 come after p 
+
+//------------------------------------------
+
+let numbers = [3, 8, 5, 6, 9]
+
+let emptyArrayTwo = []
+
+emptyArrayTwo = numbers.push(78)
+console.log(numbers)  //output 78
