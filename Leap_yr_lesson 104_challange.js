de51be78b13138c85bb7114c62a0107a98d3776c
Naomy Arnold <NaomyA@newsmax.com>
2025-01-24 11:04:51 -0500

    code maintenance

--- Leap_yr_lesson 104_challange.js
+++ Leap_yr_lesson 104_challange.js
@@ -1,27 +1,12 @@
-function isLeap(year){
-
+function isLeap(year) {
+    if (year % 4 === 0 || year % 400 === 0) {
+      console.log(' leap year');
+    } else if (year % 100 === 0) {
+      cosole.log(' not a leap year');
+    } else {
+      console.log('def not a leap year');
+    }
+  }
+  //test 2024, 2028, 2032, and 2036 are all leap years.
+  isLeap(2023);
   
-
-    if(year % 4 === 0){
-    
-        if(year % 100 === 0){
-              
-    
-            if(year % 400 === 0){
-    
-                 return "leap year";
-            }
-    }else{
-    
-           return "not leap year";
-     }
-    }else{
-    
-        return "leap year";
-    }
-    
-    
-    }
-    isLeap(1990)   
-
-    
--- if_else.js
+++ if_else.js
@@ -1,13 +1,15 @@
-let loveScore = Math.random() * 100
-loveScore = Math.floor(loveScore) + 1 
-console.log("Your love score is " + loveScore + "%") 
+// let loveScore = Math.random() * 100
+// loveScore = Math.floor(loveScore) + 1 
+// console.log("Your love score is " + loveScore + "%") 
 
-if(loveScore > 70){
-alert (" Your love score is " + loveScore + "whats up")
-if(loveScore > 30 && loveScore<= 70){
-alert (" Your love score is " + loveScore + "whats up")
-}
-}else{
-
-    alert("Your love score is " + loveScore + "whats up")
-}
+function loveScore(number) {
+    if (number > 70) {
+      console.log(' Your love score is ' + number + ' greater then 70');
+    } else if (number > 30 && number <= 70) {
+      console.log('Meets **both**  requirement since its ' + number);
+    } else {
+      console.log('Meets none of the  requirement');
+    }
+  }
+  loveScore(20);
+  
