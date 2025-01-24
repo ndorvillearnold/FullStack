3f727e7e91982067e49c00fae5614b3aef61a23d
Naomy Arnold <NaomyA@newsmax.com>
2025-01-24 12:52:50 -0500

    added while loop to fizbuzz

--- Fizzbuzz.js
+++ Fizzbuzz.js
@@ -1,37 +1,81 @@
 
 
-// I LOST jobs because of this simple problem :-) 
+//count++;  wasput in the position because its insode the while loo? 
+//How do i know exactly where to put that
 
-//write a function Every single time it will add number in sequence called output and print out what you want in console.
 
-let output = [];// we are keeping track of the numbers with this.
+/* 
+
+thats to stop code to 100 before it keeps going while(count <= 100){
+
+
+*/
+let output = [];
 let count = 1;
 
-
 function fizzBuzz(){
+
+    while(count <= 100){
+
+       if (count % 3 === 0 && count % 5 === 0){
+          output.push("FizBuzz");
+        }else if(count % 3 === 0){
+           output.push("Fizz");
+        }else if (count % 5 === 0){
+         output.push("Buzz");
+        }else{
+            output.push(count); //pushing the number to the empty array
+        }
+        
+           count++; //increment -  everytime we run it will add a  number for us to check
+            console.log(output);// just number
+    }
+    
+         console.log(output);// just number
+}
+
+fizzBuzz()
+
+
+
+/*
+------------------------------------------------------------------------------------
+BELOW IS NOT IDEAL to write fizbus because you have to keep puttin the function in console to run 
+
+ I LOST jobs because of this simple problem :-) 
+
+write a function Every single time it will add number in sequence called output and print out what you want in console.
+*/ 
+
+
+// let output = [];// we are keeping track of the numbers with this.
+// let count = 1;
+
+
+// function fizzBuzz(){
     
 
-    //% modular check remainder  if divisible by 3 it will pring Fizz and Buzz for divisible by 5
-if(count % 3 === 0 && count % 5 === 0){
-  output.push("FizBuzz");
+//     //% modular check remainder  if divisible by 3 it will pring Fizz and Buzz for divisible by 5
+// if(count % 3 === 0 && count % 5 === 0){
+//   output.push("FizBuzz");
     
-}
-else if(count % 3 === 0){
-   output.push("Fizz");
+// }
+// else if(count % 3 === 0){
+//    output.push("Fizz");
 
-}
-else if (count % 5 === 0){
- output.push("Buzz");
+// }
+// else if (count % 5 === 0){
+//  output.push("Buzz");
   
-}
-else{
+// }
+// else{
 
-    output.push(count); //pushing the number to the empty array
-}
+//     output.push(count); //pushing the number to the empty array
+// }
 
-   count++; //increment -  everytime we run it will add a  number for us to check
-    console.log(output);// just number
-}
+//    count++; //increment -  everytime we run it will add a  number for us to check
+//     console.log(output);// just number
+// }
 
 
-fizzBuzz() // to run this in the council I put FizzBuzz() and ran it there
+// fizzBuzz() // to run this in the council I put FizzBuzz() and ran it there
