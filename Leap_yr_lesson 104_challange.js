45f0dba0d4d9748cf4fa611604d487cb1740d483
Naomy <ndorvillearnold@gmail.com>
2025-01-25 12:26:14 -0500

    pulling from main

--- Leap_yr_lesson 104_challange.js
+++ Leap_yr_lesson 104_challange.js
@@ -1,27 +1,17 @@
-function isLeap(year){
+function isLeap(year) {
 
-  
+    if (year % 4 === 0 || year % 400 === 0) {
 
-    if(year % 4 === 0){
-    
-        if(year % 100 === 0){
-              
-    
-            if(year % 400 === 0){
-    
-                 return "leap year";
-            }
-    }else{
-    
-           return "not leap year";
-     }
-    }else{
-    
         return "leap year";
     }
-    
-    
-    }
-    isLeap(1990)   
+    else {
 
-    
+        return "not leap year";
+    }
+}
+
+console.log(isLeap(1990))
+console.log(isLeap(2008))
+console.log(isLeap(2012))
+
+
--- if_else.js
+++ if_else.js
@@ -1,13 +1,40 @@
-let loveScore = Math.random() * 100
-loveScore = Math.floor(loveScore) + 1 
-console.log("Your love score is " + loveScore + "%") 
-
-if(loveScore > 70){
-alert (" Your love score is " + loveScore + "whats up")
-if(loveScore > 30 && loveScore<= 70){
-alert (" Your love score is " + loveScore + "whats up")
-}
-}else{
-
-    alert("Your love score is " + loveScore + "whats up")
-}
+function ifElseFunctin(loveScore){
+    if(loveScore > 70){
+      console.log(" Your love score is " + loveScore + " whats up")
+    }else if(loveScore > 30 && loveScore <= 70){
+       console.log(" Your love score is " + loveScore + " hey")
+  
+    }else{
+       console.log("Your love score is " + loveScore + " whats popping")
+    }
+  
+  
+  }
+  
+  ifElseFunctin(30)
+  
+  
+  
+  // function ifElseFunc(loveScore){
+  //parent have to be true with nested
+  
+  
+  
+  
+  
+  //if 4 is not good then cant go to 7 because its nested
+  // if(loveScore > 70){
+  // console.log(" Your love score is " + loveScore + " whats up")
+  
+  //     if(loveScore > 30 && loveScore <= 70){
+  // console.log(" Your love score is " + loveScore + " hey")
+  // }
+  // }else{
+  
+  //     console.log("Your love score is " + loveScore + " whats popping")
+  // }
+  // }    
+  
+  // ifElseFunc(90)
+  // ifElseFunc(31)
+  // ifElseFunc(90)
