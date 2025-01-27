69157a4ef624a86ae04a51e6f1c6edf246d985a0
Naomy Arnold <NaomyA@newsmax.com>
2025-01-27 08:47:49 -0500

    added while loop

---
+++ whileloop_lesson108.js
@@ -0,0 +1,18 @@
+
+let output = [];
+let count = 1;
+
+function bottleChallenge(){
+
+while(count >= 99){
+console.log("99 nottles of bear on the wall")
+
+
+
+
+    count++;
+  }
+console.log()
+}
+
+bottleChallenge()
